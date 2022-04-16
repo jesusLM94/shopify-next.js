5ad09586ae0202e513926bef44c193160031ab92
Jesús Lizárraga <jesuslizarraga@Jesuss-MacBook-Pro.local>
2022-04-16 12:57:06 -0500

    First commit and configurations

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    SHOPIFY_STORE_DOMAIN: process.env.SHOPIFY_STORE_DOMAIN,
+    SHOPIFY_STOREFRONT_ACCESS_TOKEN: process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN,
+  },
+  images: {
+    //this is were shopify stores all of the store images
+    domains: ['cdn.shopify.com']
+  }
 }
 
 module.exports = nextConfig
