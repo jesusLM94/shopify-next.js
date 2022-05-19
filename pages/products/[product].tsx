d4aaafc57ff8360c1587eea3db4b1f9f146fb3b1
Jesús Lizárraga <jesuslizarraga@Jesuss-MacBook-Pro.local>
2022-05-18 19:22:14 -0500

    Add content to product page and create new ProductForm component

--- pages/products/[product].tsx
+++ pages/products/[product].tsx
@@ -1,5 +1,7 @@
 import React from 'react'
+import Image from 'next/image'
 import { getAllProducts, getProduct } from '../../lib/shopify'
+import ProductForm from '../../components/ProductForm'
 
 interface ProductForPaths {
   node: {
@@ -29,7 +31,20 @@ interface Product {
 }
 
 const ProductPage = ({ product }: { product: Product }) => {
-  return <div>{product.title}</div>
+  const image = product.images.edges[0].node
+
+  return (
+    <div className="min-h-screen py-12 sm:pt-20">
+      <div className="flex flex-col justify-center items-center space-y-8 md:flex-row md:items-start md:space-y-0 md:space-x-4 lg:space-x-8 max-w-6xl w-11/12 mx-auto">
+        <div className="w-full max-w-md border bg-white rounded-2xl overflow-hidden shadow-lg md:w-1/2">
+          <div className="relative h-96 w-full">
+            <Image src={image.url} alt={image.altText} layout="fill" objectFit="cover" />
+          </div>
+        </div>
+        <ProductForm />
+      </div>
+    </div>
+  )
 }
 
 export async function getStaticPaths() {
