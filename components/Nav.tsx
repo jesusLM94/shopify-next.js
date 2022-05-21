c33f24f4205d87098e2e8c42b1466d3fdb4176d5
Jesús Lizárraga <jesuslizarraga@Jesuss-MacBook-Pro.local>
2022-05-21 13:50:08 -0500

    Change first menu item

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -7,7 +7,7 @@ const Nav = () => {
       <div className="flex items-center justify-between max-w-6xl pt-4 pb-2 px-4 mx-auto lg:max-w-screen-xl">
         <Link href="/" passHref>
           <a className="cursor-pointer">
-            <span className="text-lg pt-1 font-bold">First Item</span>
+            <span className="text-lg pt-1 font-bold">BlackBuck Studio</span>
           </a>
         </Link>
         <a className="text-md font-bold cursor-pointer">Cart</a>
