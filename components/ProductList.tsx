062a3b4031eaf9b0140a0486f9a7bdbfca69a773
Jesús Lizárraga <jesuslizarraga@Jesuss-MacBook-Pro.local>
2022-05-21 13:55:09 -0500

    Use Correct Product Type

--- components/ProductList.tsx
+++ components/ProductList.tsx
@@ -1,7 +1,7 @@
-import { Product } from '../pages'
 import ProductCard from './ProductCard'
+import { CollectionProduct } from '../lib/Types'
 
-const ProductList = ({ products }: { products: Product[] }) => {
+const ProductList = ({ products }: { products: CollectionProduct[] }) => {
   return (
     <div className="bg-white">
       <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
