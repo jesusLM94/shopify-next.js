4d262e216589e947c1c6508b3849c77c90f87e2c
Jesús Lizárraga <jesuslizarraga@Jesuss-MacBook-Pro.local>
2022-05-22 20:55:22 -0500

    Add functionality to Nav so it counts items in the cart

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Link from 'next/link'
+import { CartContext } from '../context/shopContext'
 
 const Nav = () => {
+  const { cartOpen, setCartOpen, cart } = useContext(CartContext)
+
+  let cartQuantity = 0
+  cart.map((item) => {
+    return (cartQuantity += item?.variantQuantity)
+  })
+
   return (
     <header className="border-b sticky top-0 z-20 bg-white">
       <div className="flex items-center justify-between max-w-6xl pt-4 pb-2 px-4 mx-auto lg:max-w-screen-xl">
@@ -10,7 +18,9 @@ const Nav = () => {
             <span className="text-lg pt-1 font-bold">BlackBuck Studio</span>
           </a>
         </Link>
-        <a className="text-md font-bold cursor-pointer">Cart</a>
+        <a className="text-md font-bold cursor-pointer" onClick={() => setCartOpen(!cartOpen)}>
+          Cart ({cartQuantity})
+        </a>
       </div>
     </header>
   )
