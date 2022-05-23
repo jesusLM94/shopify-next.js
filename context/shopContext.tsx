3e3a1a77a8090965e258b12e9c5c63109e043147
Jesús Lizárraga <jesuslizarraga@Jesuss-MacBook-Pro.local>
2022-05-22 20:55:06 -0500

    Create context to manage Cart

---
+++ context/shopContext.tsx
@@ -0,0 +1,71 @@
+import React, { ReactElement, createContext, useState } from 'react'
+import { createCheckout, updateCheckout } from '../lib/shopify'
+
+type CartItem = {
+  id: string
+  variantQuantity: number
+}
+
+interface CartContextI {
+  cart: CartItem[]
+  cartOpen: boolean
+  setCartOpen: (cartOpen: boolean) => void
+  addToCart: (newItem: CartItem) => void
+  checkoutUrl: string
+}
+
+const CartContext = createContext<CartContextI>({
+  cart: [],
+  cartOpen: false,
+  setCartOpen: () => {},
+  addToCart: () => {},
+  checkoutUrl: '',
+})
+
+export default function ShopProvider({ children }: { children: ReactElement }) {
+  const [cart, setCart] = useState<CartItem[]>([])
+  const [cartOpen, setCartOpen] = useState<boolean>(false)
+  const [checkoutId, setCheckoutId] = useState<string>('')
+  const [checkoutUrl, setCheckoutUrl] = useState<string>('')
+
+  const addToCart = async (newItem: CartItem) => {
+    if (cart.length === 0) {
+      setCart([newItem])
+      const checkout = await createCheckout(newItem.id, newItem.variantQuantity)
+
+      setCheckoutId(checkout.id)
+      setCheckoutUrl(checkout.webUrl)
+
+      localStorage.setItem('checkout_id', JSON.stringify([newItem, checkout]))
+    } else {
+      let newCart: { id: string; variantQuantity: number }[] = []
+      let added = false
+
+      cart.map((item) => {
+        if (item.id === newItem.id) {
+          item.variantQuantity++
+          newCart = [...cart]
+          added = true
+        }
+      })
+
+      if (!added) {
+        newCart = [...cart, newItem]
+      }
+
+      setCart(newCart)
+      const newCheckout = await updateCheckout(checkoutId, newCart)
+      localStorage.setItem('checkout_id', JSON.stringify([newCart, newCheckout]))
+    }
+  }
+
+  return (
+    <CartContext.Provider value={{ cart, cartOpen, setCartOpen, addToCart, checkoutUrl }}>
+      {children}
+    </CartContext.Provider>
+  )
+}
+
+const shopConsumer = CartContext.Consumer
+
+export { CartContext, shopConsumer }
